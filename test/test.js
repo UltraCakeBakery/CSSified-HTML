354c25ef85f6a578b9284d06a32086ae4dcddd43
Jack van der Bilt <jack@managing.software>
2022-08-22 14:40:23 +0200

    feat: arbitrary suffixes

--- test/test.js
+++ test/test.js
@@ -4,10 +4,12 @@ import generate from '../packages/core/dist/index.js'
 const html = fs.readFileSync('./test.html').toString()
 
 const interations = 100_000
-const generated = generate(html)
-const time = new Date()
 
-// for( let i = 0; i < interations; i++ ) generate(html)
+const time = performance.now()
+const generated = generate(html)
+const finalTime = performance.now()
+console.log(generated)
+console.log(finalTime - time)
 
 // console.log( 'generated css: ' + (( new Date() - time ) / interations ) + 'ms average' )
-console.log(generated)
+
