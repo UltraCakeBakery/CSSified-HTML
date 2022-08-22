16c2e9a436eef302c813b737b20e4a89022bc65b
Jack van der Bilt <jack@managing.software>
2022-08-22 21:36:25 +0200

    refactor: performance

--- test/test.js
+++ test/test.js
@@ -3,13 +3,8 @@ import generate from '../packages/core/dist/index.js'
 
 const html = fs.readFileSync('./test.html').toString()
 
-const interations = 100_000
-
 const time = performance.now()
-const generated = generate(html)
+generate(html)
 const finalTime = performance.now()
-console.log(generated)
-console.log(finalTime - time)
-
-// console.log( 'generated css: ' + (( new Date() - time ) / interations ) + 'ms average' )
+console.log(( finalTime - time ))
 
