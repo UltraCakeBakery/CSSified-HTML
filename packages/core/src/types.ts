a26c2b4f68931d52e965ea7c76b0b28b1cacac21
Jack van der Bilt <jack@managing.software>
2022-08-23 10:42:16 +0200

    fix: duplicated font weights in generated google fonts url

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -1,6 +1,6 @@
 export interface Map {
     imports: [string] | never[], 
-    fonts: { [key: string]: { weights: string[], styles: string[] } },
+    fonts: { [key: string]: { [key: string]: Set<string> } },
     medias: { [key: string]: { [key: string]: string[] } },
     propertyCounts: { [key: string]: number }
 }
